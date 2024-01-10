6b5c38103a8f60ca0355c773ef94ae8b3a1d9c7f
Abraham <96045795+AbrahamRajoRodriguez@users.noreply.github.com>
2024-01-10 11:39:43 +0100

    primerCommit

---
+++ ProyectoFinal/PROYECTO/database/migrations/2024_01_10_094919_create_webs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id'); // ID del cliente (clave externa)
+            $table->foreign('client_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webs');
+    }
+}
